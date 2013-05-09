a6ff98653b4de07b95d6fdd7dddf153087fb2265
RYO NAGAI <seinaoasis@gmail.com>
2013-05-09 18:48:03 +0900

    フッター文言の削除

--- themes/blogum/footer.php
+++ themes/blogum/footer.php
@@ -1,8 +1,8 @@
             </div>
         </div>
         <div class="footer">
-            <p class="copy">&copy; 2010 —<a href="<?php bloginfo('home'); ?>"><?php bloginfo('name'); ?></a>. All Rights Reserved. Powered by <a href="http://wordpress.org" target="_blank">Wordpress</a>.</p>
-            <p class="credits">Designed by <a href="http://wpshower.com" target="_blank">Wpshower</a></p>
+            <p class="copy">&copy; 2013-<a href="<?php bloginfo('home'); ?>"><?php bloginfo('name'); ?></a><a href="http://wordpress.org" target="_blank"></a>.</p>
+            <p class="credits"><a href="http://wpshower.com" target="_blank"></a></p>
         </div>
 
         <!-- Page generated: <?php timer_stop(1); ?> s, <?php echo get_num_queries(); ?> queries -->
