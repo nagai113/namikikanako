1950c73fc0ef4507b0f5520d1b968008fea5b5e9
RYO NAGAI <seinaoasis@gmail.com>
2013-05-10 13:41:02 +0900

    投稿者IDの削除、投稿日付の並び順変更、続きを読む

--- themes/blogum/loop.php
+++ themes/blogum/loop.php
@@ -5,14 +5,13 @@
             <div class="post_meta">
                 <h2><a href="<?php the_permalink() ?>" title="<?php printf( esc_attr__('Permalink to %s'), the_title_attribute('echo=0') ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
                 <div class="post_data">
-                    <div class="post_author">By: <?php the_author_link() ?></div>
-                    <div class="post_date"><?php the_time(__('d-m-Y')) ?></div>
+                    <div class="post_date"><?php the_time(__('Y-m-d')) ?></div>
                     <div class="post_categories"><?php the_category(', '); ?></div>
                 </div>
             </div>
             <div class="post_content">
                 <?php the_content(false); ?>
-                <a href="<?php the_permalink() ?>#more" class="more">Read More</a>
+                <a href="<?php the_permalink() ?>#more" class="more">続きを読む...</a>
             </div>
         </div>
 
